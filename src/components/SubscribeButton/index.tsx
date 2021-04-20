327a0f5d4a14850ac6418a4f2523d85484c40b38
Pablo Giaccon <psjacon1995@gmail.com>
2021-04-20 18:34:07 -0300

    feat: faundadb stripe subscriptions

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,44 @@
-import styles from './styles.module.scss'
+import { useSession, signIn } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
 interface SubscribeButton {
   priceId: string;
 }
 
-export function SubscribeButton({priceId}:SubscribeButton) {
+export function SubscribeButton({ priceId }: SubscribeButton) {
+  const [session,] = useSession()
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+      console.log(response)
+
+      const {sessionId} = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId})
+
+    } catch(err) {
+      alert(err.message);
+    }
+
+  }
+
   return (
     <button
-    type="button"
-    className={styles.subscribeButton}
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
-Subscribe now
+      Subscribe now
     </button>
-  )
+  );
 }
